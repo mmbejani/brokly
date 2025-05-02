fb405dd798799f29ac038c17445ad2e46a1b4b43
Mahdi <mohammmad.mahdi.bejani@gmail.com>
2025-05-02 14:02:01 +0400

    add generic asseration for basic functions

--- include/tensor/Tensor.hh
+++ include/tensor/Tensor.hh
@@ -1,9 +1,10 @@
 #pragma once
 
+#include <list>
 #include <map>
 
 #include "tensor/Define.hh"
-#include "tensor/autograd/Common.hh"
+#include "tensor/autograd/common.hh"
 
 namespace momas::brokly::tensor {
 
@@ -25,21 +26,32 @@ template <typename T> class Tensor {
 
 public:
   Tensor(dim4 dimension, bool requires_grad = false,
-         std::map<autograd::NamedTensor, Tensor<T> *> (*grad_fn)(
-             std::map<autograd::NamedTensor, Tensor<T> *>) = nullptr,
-         T *data = nullptr)
+         void (*grad_fn)(Tensor<T> *) = nullptr, T *data = nullptr)
       : dimension(dimension), grad_fn(grad_fn), requires_grad(requires_grad),
-        data(data) {
+        data(data), grad(nullptr) {
 
     if (this->data == nullptr) {
       this->data = (T *)aligned_alloc(AVX_MEM_ALIGNED,
                                       this->dimension.total * sizeof(T));
       std::memcpy(this->data, data, sizeof(T) * dimension.total);
     }
+    this->single = dimension.total == 1;
   }
 
-  inline void backward() {
-    this->backwardHooks = this->grad_fn(this->forwardHooks);
+  inline bool is_leaf() const { return this->grad_fn == nullptr; }
+
+  void backward() {
+    for (const Tensor<T>* &backwardTensor : this->backwardHooks) {
+      if (backwardTensor->grad == nullptr) {
+        return;
+      }
+    }
+    this->grad_fn(this);
+    for (const auto &[name, forwardTensor] : this->forwardHooks) {
+      if (forwardTensor->required_grad) {
+        forwardTensor->backward();
+      }
+    }
   }
 
 public:
@@ -47,9 +59,9 @@ public:
   T *data;
   Tensor<T> *grad;
 
-  bool requires_grad;
-  std::map<autograd::NamedTensor, Tensor<T> *> forwardHooks, backwardHooks;
-  std::map<autograd::NamedTensor, Tensor<T> *> (*grad_fn)(
-      std::map<autograd::NamedTensor, Tensor<T> *>);
+  bool requires_grad, single;
+  std::map<autograd::NamedTensor, Tensor<T> *> forwardHooks;
+  std::list<Tensor<T> *> backwardHooks;
+  void (*grad_fn)(Tensor<T> *);
 };
 } // namespace momas::brokly::tensor
